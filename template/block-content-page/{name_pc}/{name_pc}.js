9172d02a21f7054be6f26c9d94a285ba05dd52ac
Lars van Braam <lars@mediamonks.com>
2017-07-21 16:28:02 +0200

    Add a template for a content page Re-render the docs Update README.md

---
+++ template/block-content-page/{name_pc}/{name_pc}.js
@@ -0,0 +1,13 @@
+import { AbstractContentPageComponent }from 'vue-block-system';
+import {{name_pc}}TransitionController from './{{name_pc}}TransitionController';
+
+export default {
+	name: '{{name_pc}}',
+	extends: AbstractContentPageComponent,
+	methods: {
+		handleAllComponentsReady() {
+			this.transitionController = new {{name_pc}}TransitionController(this);
+			this.isReady();
+		},
+	},
+};
---
+++ template/block-content-page/{name_pc}/{name_pc}.vue
@@ -0,0 +1,16 @@
+<style src="./{{name_pc}}.scss" module lang="scss"></style>
+<script src="./{{name_pc}}.js"></script>
+
+<template>
+	<div>
+		<component
+			v-for="(block, index) in blocks"
+			@isReady="handleBlockComponentReady"
+			:scrollId="block.scrollId"
+			:data="block.data"
+			:debugLabel="true"
+			:is="block.id"
+			:componentId="block.id + '.' + block.blockIndex"
+			:key="index"></component>
+	</div>
+</template>
