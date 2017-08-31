4dc02e5d5fb5654ac2a48b4f31daf3a09d50530b
Lars van Braam <lars@mediamonks.com>
2017-08-31 16:12:07 +0200

    Update demo page source code

--- demo/src/block/BlockBar/BlockBar.js
+++ demo/src/block/BlockBar/BlockBar.js
@@ -1,9 +1,14 @@
+import VueTypes from 'vue-types';
 import { AbstractBlockComponent } from 'vue-block-system';
 import BlockBarTransitionController from './BlockBarTransitionController';
+import BlockBarData from './BlockBarData';
 
 export default {
 	name: 'BlockBar',
 	extends: AbstractBlockComponent,
+	props: {
+		data: VueTypes.shape(BlockBarData).isRequired,
+	},
 	methods: {
 		handleAllComponentsReady() {
 			this.transitionController = new BlockBarTransitionController(this);
---
+++ demo/src/block/BlockFoo/BlockFooData.js
@@ -0,0 +1,6 @@
+import VueTypes from 'vue-types';
+
+export default {
+	id: VueTypes.string.isRequired,
+	heading: VueTypes.string.isRequired,
+};
