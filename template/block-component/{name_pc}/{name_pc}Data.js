0d27277d6eb9039e39cd0904721b3fe5e3b220f2
Lars van Braam <lars@mediamonks.com>
2017-11-20 15:38:35 +0100

    Update the template for the block components

--- template/block-component/{name_pc}/{name_pc}Data.js
+++ template/block-component/{name_pc}/{name_pc}Data.js
@@ -1,3 +1,9 @@
-export default {
+import VueTypes from 'vue-types';
 
+/**
+ * @param {description} scrollId The unique id of the block, used for scrolling to blocks by hashes
+ * @param {placeholder} scrollId unique-block-id
+ */
+export default {
+	scrollId: VueTypes.string,
 };
